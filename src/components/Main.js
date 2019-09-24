210c5ff0252fca9ea920d1d3cb606e9da2ceac63
Richard Chen <chenrich11@gmail.com>
2019-09-24 19:06:41 -0400

    added linked in

--- src/components/Main.js
+++ src/components/Main.js
@@ -42,7 +42,7 @@ class Main extends React.Component {
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
           <ul className="icons">
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="#" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
             <li><a href="https://www.instagram.com/chen_is_rich/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
             <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
           </ul>
