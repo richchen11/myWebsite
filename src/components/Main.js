9263f5a0b790d1e6dc4f32dec320c78a5ca09ccb
Richard Chen <chenrich11@gmail.com>
2019-09-24 14:20:22 -0400

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -59,7 +59,7 @@ class Main extends React.Component {
             <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
             <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
             <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="https://www.instagram.com/chen_is_rich/" className="icon fa-github"><span className="label">GitHub</span></a></li>
           </ul>
           {close}
         </article>
